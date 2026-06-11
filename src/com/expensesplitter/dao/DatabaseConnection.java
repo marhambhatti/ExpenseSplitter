50fb642cfff8e8c253b3f9e1700e9b09ddb04bb0
Arham Bhatti <muhammadarham0372@gmail.com>
2026-06-11 18:22:15 +0500

    Dataase Connection Establishment SuccessFuly

--- src/com/expensesplitter/dao/DatabaseConnection.java
+++ src/com/expensesplitter/dao/DatabaseConnection.java
@@ -10,10 +10,12 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL      = "jdbc:mysql://localhost:3306/expense_splitter_db";
-    private static final String USER     = "root";   // change if needed
-    private static final String PASSWORD = "";        // change if needed
-
+    private static final String USER     = "root";
+    private static final String PASSWORD = "";
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    public static void main(String [] args){
+        System.out.println("Database Connection SuccessFully");
     }
 }
