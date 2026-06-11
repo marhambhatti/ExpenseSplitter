1012aa73ca398e0bfb059761fb42a734a494b9c8
Arham Bhatti <muhammadarham0372@gmail.com>
2026-06-11 18:44:29 +0500

    DataBase Connection Error Root Password resolve

--- src/com/expensesplitter/database/DatabaseConnection.java
+++ src/com/expensesplitter/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL      = "jdbc:mysql://localhost:3306/expense_splitter_db";
     private static final String USER     = "root";
-    private static final String PASSWORD = "your_password_here";
+    private static final String PASSWORD = "";
 
     private static Connection connection = null;
 
