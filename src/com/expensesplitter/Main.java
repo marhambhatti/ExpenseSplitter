a6f444a9b15e3fbf35635d5e750c44b86c09e3cb
Arham Bhatti <muhammadarham0372@gmail.com>
2026-06-13 18:57:50 +0500

    Main Class File Created

--- src/com/expensesplitter/Main.java
+++ src/com/expensesplitter/Main.java
@@ -3,11 +3,16 @@ package com.expensesplitter;
 import com.expensesplitter.ui.LoginFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-
+// Main Class
 public class Main {
     public static void main(String[] args) {
-        try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
-        catch (Exception e) { e.printStackTrace(); }
+        // Running UI (Log In Screen Shows First)
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
     }
 }
